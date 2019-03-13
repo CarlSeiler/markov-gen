526103eda0ccaf0e114b8e9f208c9981ac4b7b58
Carl Seiler <cseilergithub@overanalysis.org>
2019-03-13 10:23:44 -0500

    First Deployment

--- app.js
+++ app.js
@@ -1,58 +1,175 @@
 // 
-const fs = require ('fs');
-const stream = require('stream');
-var output = [];
-var dictionary = new Map();
+const fs = require('fs');
+//const stream = require('stream');
+var theText = [];
 var regex = /\s+/;
+var isDictionaryComplete = false;
 
-const order = 4;
-const inputFile = "input.txt";
+const order = 3;
+const group_size = order + 1;
+const inputFile = "buckle.txt";
+const theLength = 50; // Length of the generated output
 
+// markovDictionary = new Map();
 
-fs.readFile(inputFile,'utf8', (err, contents) => {
-    if (err) {
-        console.log (err.message);
-        console.log ("Problem reading input.");
-    } else {
-    output = contents.split(regex);
-    console.log("Input read from " + inputFile + '.');
-    // put the tail on the end for needed order
-    var i;
-    for (i = 0; i < order; i++ ) {
-        output.push(output[i]);
-    }
-    console.log ('Array of keys: ' + output);
-    output.forEach ((element,outputIndex) => {
-        // Do not create dictionary entries for items added to the tail
-        if (outputIndex <= output.length-(order+1)) { 
-        var dictValue = [];
-        var newItem = [];
-        if (dictionary.has(element)) {
-            console.log('Element exists, adding value to key.');
-            newItem =  fillElements(output,outputIndex);
-            dictValue = dictionary.get(element);
-            dictValue.push(newItem);
-            dictionary.set(element, dictValue);
+function train (textArray, filename, isDictFinished, myCallback) {
+    var dictionary = new Map();
+    fs.readFile(filename, 'utf8', (err, contents) => {
+        
+        console.log (`Reading input file '${filename}'`);
+        if (err) {
+            console.log(err.message);
+            console.log("Problem reading input.");
         } else {
-            console.log('Element does not exist in dictionary, adding key-value pair.');
-            dictionary.set (element,  [fillElements(output,outputIndex)] );
+            textArray = contents.split(regex);
+            console.log(`Finished reading from '${filename}'`);
+            // put the tail on the end for needed order
+            var i;
+            for (i = 0; i < order; i++) {
+                textArray.push(textArray[i]);
+            }
+            //console.log ('Array of keys: ' + output);
+            var itemsProcessed = 0;
+            console.log ("Beginning processing and building dictionary.");
+            //in python, this would use tuples as the key.  We have to kind of force things here to use
+            // arrays of length of the order as the key.
+            textArray.forEach((element, textIndex, theArray) => {
+                // Create dictionary entries for all items except those in the tail
+                if (textIndex <= textArray.length - (group_size)) {
+                    var dictValue = []; // Each KEY is an array of length order and has has VALUE of
+                    var newItem = "";
+                    // TODO For each element in the text array, build a dictionary KEY that constists
+                    // of an array of length order. Check to seee if it is already in the dictionary.
+                    // If it is, push it on the array that holds the VALUEs. If it doesn't exist
+                    // create the entry and add a single item array to the VALUEs.
+                    // BUILD KEY:
+                    var currentKey = buildKey(theArray, textIndex);
+                    
+                    if (dictionary.has(currentKey)) {
+                        // console.log('Item exists, adding value to key.');
+                        newItem = theArray[textIndex+order];
+                         dictValue = dictionary.get(currentKey);
+                         dictValue.push(newItem);
+                         // console.log (`Adding value ${newItem} to ${currentKey}`);
+                         dictionary.set(currentKey, dictValue);
+                    } else {
+                        newItem = theArray[textIndex+order];
+                        // console.log(`Item ${currentKey} does not exist.  Added entry with value ${newItem}`);
+                        dictionary.set(currentKey, [newItem] );
+                    }
+                    function buildKey(textArray, currentIndex) {
+                        // They KEY will consist of the current item in textArray array plus however many items in our 
+                        // order.  So for order 0, it will consist only of the item at that index of theText.
+                        // For order 2, it will consist of the item at the index and the next two items, etc.
+                        var j ;
+                        thisKeyString = "";
+                        thisKeyArray =[];
+                        for (j = currentIndex; j < (currentIndex+order); j++) {
+                            thisKeyArray.push(textArray[j]);
+                            //thisKeyString = thisKeyString + " " + textArray[j]                    }
+                        }
+                        thisKeyString = thisKeyArray.join(" ");
+                        return thisKeyString;
+                    }
+                }
+                itemsProcessed++;
+                if (itemsProcessed === theArray.length) {
+                    console.log ("Dictionary build complete.");
+                    callback (isDictFinished);
+                }
+    
+            });
+            //console.log (dictionary);
+            //console.log (dictionary.size);
+            //this originally could have failed because Array.forEach technically does not provide call back and could theoretically 
+            // still be working when the forEach and have moved on to this.    This has been re-written to hopefully take
+            // advantage of this situation:
+            // https://stackoverflow.com/questions/18983138/callback-after-all-asynchronous-foreach-callbacks-are-completed
+            //
+            function callback() {
+                isDictFinished = true;
+                myCallback();
+                
+                // // Parsing test:
+                // var x = "Humboldt";
+                // if (dictionary.has(x)) {
+                //     console.log(dictionary.get(x));
+                //     console.log(dictionary.get(x).length);
+                // }
+                // else {
+                //     console.log('Not found.');
+                // }
+            
+    
+            }
+    
         }
-    function fillElements(o,oi) {
-        var j;
-        arrOfValues = [];
-        for (j = 1; j <= order; j++) {
-            arrOfValues.push(o[oi+j]);
+    });
+    return dictionary; 
+    }
+var markovDictionary = train(theText,inputFile, isDictionaryComplete, finalresult);
+
+
+
+
+function generate (dict, len) {
+    // As a place to start, pick a random set of ORDER number of words and store them in RESULT
+    //
+    if (group_size < theLength) {
+        var startingIndex = Math.floor(Math.random() * dict.size);
+        var next_word = ""; // Randomly selected next word from values based on given key
+        var result = Array.from(dict.keys())[startingIndex];
+        // console.log (`Key at startingIndex:${result}`);
+        // Look up that KEY in the dictionary and randomly pick the next word, and stick it on RESULT
+        next_word = sample(dict.get(result));
+        result = result + " " + next_word;
+        // Easier to work with arrays, so make this an array really quick
+        var result_array = result.split(" ");
+        //setup our initial state
+        var state = result_array.slice(result_array.length - order );
+        console.log (`Current state:${state}`);
+        //console.log (`Current result:${result_array.join(' ')}`);
+        while (result_array.length < theLength) {
+            next_word = sample(dict.get(state.join(' ')));
+            result_array.push(next_word);
+            state = result_array.slice(result_array.length - order );
         }
-        return arrOfValues;
-
+        console.log ("Final results: " + result_array.join(' '));
+        fs.writeFile("output.txt", result_array.join(' '), (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            else
+            {
+                console.log('File saved as output.txt');
+            }
+        });   
+        
+        
+    } else {
+        console.log (`The group_size must be smaller than theLength to be able to run.`);
     }
-    }
-       
-});
-console.log (dictionary);
+    // console.log (result.typeof);
+    // var state = result; 
+    // //console.log (result + ` and len = ${len}`);
+    // for (i = 1; i <= len; i++) {
+    //     next_word = dict.get(state)[Math.floor(Math.random() * dict.get(state).length)];
+    //     result.push(next_word);
+    //     state = result.slice(-result.length-order );
+    // }
+    // console.log (result);
+};
+function sample(array) {
+    // Takes an array and returns a random element 
+    // This should be a string in this case.
+    return array[Math.floor (Math.random() * array.length)];
+}
 
 
-    }
-});
+function finalresult() {
+        //
+        generate (markovDictionary, 1);    
+        console.log ('\nDone.');
 
+        } 
 
